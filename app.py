d80f4dc28d32f8da634251f3c29aa221a84f60f9
Sarath Chandra <124865838+Sarathchandra-Janapati@users.noreply.github.com>
2025-07-19 16:22:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,6 @@ def ask():
 
     return jsonify({"response": response_text, "audio_url": audio_url})
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
