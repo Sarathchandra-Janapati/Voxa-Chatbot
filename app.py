5daa5cb90eb3fc027c075e85c79a2e50e2736590
Sarath Chandra <124865838+Sarathchandra-Janapati@users.noreply.github.com>
2025-07-19 13:51:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def ask():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render will set the PORT env variable
+    app.run(host="0.0.0.0", port=port)
